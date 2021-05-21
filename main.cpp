e4b1628936f4c7c5a98df9b319dc34e677c6fbcb
alanrazop <83654918+alanrazop@users.noreply.github.com>
2021-05-21 00:36:02 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//
+// Proyecto A01703350 main
+// Alan Fernando Razo Peña
+// A01703350
+// 21/05/2021
+// Este es el proyecto integrador para la clase TC1030 Programación Orientado a Objetos. Es un programa que captura diferentes tipos de empleado con sus respectivos sueldos, y nos permite calcular la nómina para cada tipo de empleado diferente.
+//
+
+#include <iostream>   
+#include "Docente.h" // incluir bibliotecas
+#include "Escuela.h"
+
+using namespace std;
+
+int main() {
+    Escuela e;
+    e.ingresaInfo();
+    return 0;
+}
