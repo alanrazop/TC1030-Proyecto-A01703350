063b334670a1f686086975cf676620e4b4856b7f
Alan Razo <83654918+alanrazop@users.noreply.github.com>
2021-05-25 21:33:34 -0500

    Update Escuela.h

--- Escuela.h
+++ Escuela.h
@@ -3,140 +3,118 @@
 // Alan Fernando Razo Peña
 // A01703350
 // 21/05/2021
-// Esta clase Escuela contiene todas las operaciones para hacer altas de información para grupos, alumnos y asignaturas, y es utilizada por la función main del programa, parte del proyecto A01703350.
+// Esta clase Escuela contiene todas las operaciones para hacer altas de información para la escuela, y es utilizada por la función main del programa, parte del proyecto A01703350.
 //
-
 #ifndef ESCUELA_H
 #define ESCUELA_H
 
-#include <string>
 #include <iostream>
 #include <sstream>
-#include "Docente.h" //biblioteca con los objetos que se van a usar
+#include "Miembro.h" //biblioteca con los objetos que se van a usar
 
 using namespace std;
+
+const int MAX=1000;
 
 class Escuela {
 
     //Declarar variables privadas(atributos)
     private:
-        int agregarDatos;
+        string nombre;
+        string direccion;
+        string numero;
+        string ciudad;
+        string pais;
+        Miembro* mie[MAX];
+        int persona;
     //Declaro constructor por default y metodos públicos.
     public:
-    Escuela(){
-      this -> agregarDatos = 0;
-    }
-    int get_agregarDatos(){
-        return this -> agregarDatos;
-    }
-    void set_agregarDatos(int _agregarDatos){
-        this -> agregarDatos = _agregarDatos;
-    }
-    void ingresaInfo(){
-        int verificar;
-        cout << "Ingrese la sección a la que quiera acceder del sistema" << endl;
-        cout << "1.Grupos  2. Alumnos 3. Asignaturas" << endl;
-        cin >> verificar;
-        if (verificar == 1){
-          int ingresaGrupo;
-          int verificarImpresion;
-          cout << "¿Cuántos grupos quiere ingresar al sistema?" << endl;
-          cin >> ingresaGrupo
-          Docente *Grupos[ingresaGrupo];
-          for (int i=0;i<ingresaGrupo;i++){
-            int integrantes;
-            string nombre;
-            int salon;
-            cout << "¿Cuantos integrantes hay en el salón?" << endl;
-            cin >> integrantes;
-            cout << "¿Cuál es el nombre del salón?" << endl;
-            cin >> nombre;
-            cout << "¿Qué número de salón es?" << endl;
-            cin >> salon;
-            Grupos[i] = new Grupos(integrantes, nombre, salon);
-          }
-          cout << "¿Quiere imprimir su información?   1. SI  2. NO" << endl;
-          cin >> verificarImpresion;
-          if (verificarImpresion == 1){
-            for (int i = 0; i<ingresaGrupo;i++){
-              Grupos[i]->mostrar();
-              cout << "\n" << endl;
-            }
-          }
-          else{
-            cout << "Invalidando datos registrados" << endl;
-            }
-        }
-        else if (verificar == 2){
-          int ingresaAlumno;
-          int verificarImpresion;
-          cout << "¿Cuántos alumnos quiere ingresar al sistema?" << endl;
-          cin >> ingresaAlumno;
-          Docente *Alumnos[ingresaAlumno];
-          for (int i=0;i<ingresaAlumno;i++){
-            int id;
-            int matricula;
-            string nombre;
-            string email;
-            int edad;
-            cout << "¿Cuál es el id del estudiante?" << endl;
-            cin >> id;
-            cout << "¿Cuál es la matricula del estudiante?" << endl;
-            cin >> matricula;
-            cout << "¿Cuál es el nombre del estudiante?" << endl;
-            cin >> nombre;
-            cout << "¿Cuál es la edad del estudiante?" << endl;
-            cin >> edad;
-            cout << "¿Cuál es su email del estudiante?" << endl;
-            cin >> email;
-            Alumnos[i] = new Alumnos(id, matricula, nombre, edad, email);
-          }
-          cout << "¿Quiere imprimir su información?   1. SI  2. NO" << endl;
-          cin >> verificarImpresion;
-          if (verificarImpresion == 1){
-            for (int i = 2; i<ingresaAlumno;i++){
-              Alumnos[i]->mostrar();
-              cout << "\n" << endl;
-            }
-          else{
-            cout << "Invalidando datos registrados" << endl;
-             }
-          }
-          else if (verificar == 3){
-          int ingresaAsignatura;
-          int verificarImpresion;
-          cout << "¿Cuántas asignaturas quiere ingresar al sistema?" << endl;
-          cin >> ingresaAsignatura;
-          Docente *Asignaturas[ingresaAsignatura];
-          for (int i=0;i<ingresaAsignatura;i++){
-            int id;
-            string nombre;
-            int creditos;
-            int unidades;
-            cout << "¿Cuál es el id de la asignatura?" << endl;
-            cin >> id;
-            cout << "¿Cuál es la matricula de la asignatura?" << endl;
-            cin >> matricula;
-            cout << "¿Cuál es el nombre de la asignatura?" << endl;
-            cin >> nombre;
-            cout << "¿Cuantas unidades tiene la asignatura?" << endl;
-            cin<< unidades;
-            Asignaturas[i] = new Asignaturas(id, matricula, nombre, edad, email);
-          }
-          cout << "¿Quiere imprimir su información?   1. SI  2. NO" << endl;
-          cin >> verificarImpresion;
-          if (verificarImpresion == 1){
-            for (int i = 2; i<ingresaAsignatura;i++){
-              Asignaturas[i]->mostrar();
-              cout << "\n" << endl;
-            }
-          else{
-            cout << "Invalidando datos registrados" << endl;
-          }
-        }
+
+    
+      Escuela(): persona(0) {}; //constructor por defualt
+
+      void crea_ejemplos() {
+        //new crea el objeto en tiempo de ejecución para usar polimorfismo
+        mie[persona]=new Alumno(persona,"Alan","alanrazo2000@hotmail.com","A01703350", 90.5);
+        persona++;
+        mie[persona]=new Alumno(persona,"Arturo","arturo@hotmail.com","A017037645", 95.7);
+        persona++;
+        mie[persona]=new Profesor(persona,"Anna","soyana@hotmail.com","Artes visuales");
+        persona++;
+        mie[persona]=new Profesor(persona,"Andrea","andyyy@hotmail.com","Filosofía");
+        persona++;
+        mie[persona]=new Familiar(persona,"Alejandro","alex99@hotmail.com","Padre", 50);
+        persona++;
+        mie[persona]=new Familiar(persona,"Ariel","lasirenita@hotmail.com","Madre", 45);
+        persona++;
       }
-    }
-  }
+      void muestra_miembros() {
+        for (int i = 0; i < persona; i++)
+        mie[i]->mostrar();
+        cout << "\n" << endl;
+      }
+
+string getNombre(){
+  return nombre;
+}
+string getDireccion(){
+  return direccion;
+}
+string getNumero(){
+  return numero;
+}
+string getCiudad(){
+  return ciudad;
+}
+string getPais(){
+  return pais;
+}
+void setNombre(string _nombre){
+  nombre=_nombre;
+}
+void setDireccion(string _direccion){
+  direccion=_direccion;
+}
+void setNumero(string _numero){
+  numero=_numero;
+}
+void setCiudad(string _ciudad){
+  ciudad=_ciudad;
+}
+void setPais(string _pais){
+  pais=_pais;
 }
 
-#endif /* ESCUELA_H */
+void registrarInfo(){
+  string nombre;
+  string direccion;
+  string numero;
+  string ciudad;
+  string pais;
+  cout << "Nombre de la escuela: " << endl;
+  getline(cin, nombre);
+  setNombre(nombre);
+  cout << "Dirección de la escuela: " << endl;
+  getline(cin, direccion);
+  setDireccion(direccion);
+  cout << "Número exterior: " << endl;
+  getline(cin, numero);
+  setNumero(numero);
+  cout << "Ciudad: " << endl;
+  getline(cin, ciudad);
+  setCiudad(ciudad);
+  cout << "Pais: " << endl;
+  getline(cin, pais);
+  setPais(pais);
+}
+
+void imprimeInfo(){
+  cout << "Bienvenidos a " << getNombre() << endl;
+  cout << "Ubicada en " << getDireccion() << endl;
+  cout << "Número: " << getNumero() << endl;
+  cout << "En la ciudad de: " << getCiudad() << endl;
+  cout << "Del país: " << getPais() << endl;
+}
+
+};
+#endif /*ESCUELA_H*/
